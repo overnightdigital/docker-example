41a68f2de7e9f36eeeaae41e2e2be77677a0fe76
Aleksandar Milinkovic <milinkovicaleks@gmail.com>
2020-10-30 17:47:28 +0100

    connected frontend with backend and aded nginx server

--- server/index.js
+++ server/index.js
@@ -7,15 +7,16 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Express route handlers
-app.get('/', (req, res) => {
-  res.send({
+app.get('/data', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({
     id: "id-123456",
     name: "Agent 56",
     content: "Case 346"
-  });
+  }));
 });
 
 
 app.listen(5000, (err) => {
-  console.log('Listening');
+  console.log('Listening on Port 5000');
 });
